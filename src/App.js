c30fa01efeaf7af1e0694b4d38346051d06622cf
yasutakatou <katou.pfu@jp.fujitsu.com>
2020-01-31 10:12:19 +0900

    fix (text area couldn't rewrite.)

--- src/App.js
+++ src/App.js
@@ -4,30 +4,28 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      mail:""
+      mail:"test@test.com"
     }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event) => {
+    this.setState({mail: event.target.value})
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <label>
-            mail: 
-            <input type="text" name="mail" onChange={this.handleChange} value={this.state.mail}/>
-          </label>
-          <br />
-          <button onClick={this.handleClick}>Start QR Scan</button>
-        </header>
+      <div>
+        mail: 
+        <input type="text" name="mail" onChange={this.handleChange} value={this.state.mail}/>
+        <br />
+        <button onClick={this.handleClick}>Start QR Scan</button>
       </div>
     )
   }
 
-  handleChange = (event) => {
-    this.setState({[event.target.mail]: event.target.value})
-  }
-
   handleClick = (event) => {
+    event.preventDefault();
     const mailAddress = this.state.mail
     window['cordova'].plugins.barcodeScanner.scan(
       function (result) {
@@ -42,7 +40,7 @@ class App extends Component {
               'Accept': 'application/json',
               'Content-Type': 'application/json'
             };
-            fetch(url, {method, headers, body}).then((res)=> res.json()).then(alert).catch(alert);
+            fetch(url, {method, headers, body}).then((res)=> res.json()).then(alert).catch(alert);            
           }
       },
       function (error) {
